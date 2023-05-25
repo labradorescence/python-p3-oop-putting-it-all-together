9f6bcef806ccb350fcbc5d0ee6630e95275e9fa3
labradorescence <orangenewyorkcreativeagency@gmail.com>
2023-05-25 16:46:12 -0400

    oop

--- lib/book.py
+++ lib/book.py
@@ -1,4 +1,56 @@
 #!/usr/bin/env python3
 
 class Book:
+
+    def __init__(self, title, page_count = 0): #init method
+        self.title = title #instance attribute
+        self._page_count = page_count
     pass
+
+    def get_author(self):
+        return self._author #get instance property
+
+    def set_author(self, author):
+        self._author = author #set instance property
+
+    author = property(get_author, set_author) #compiling the getter / setter 
+
+
+    def get_page_count(self):
+        return self._page_count
+    
+    def set_page_count(self, page_count):
+        #option 1
+        if isinstance(page_count, int):
+            self._page_count = page_count
+        else:
+            print("page_count must be an integer")
+
+        #option 2 
+        # if type(page_count) == int:
+        #     self._page_count = page_count
+    
+    page_count = property(get_page_count, set_page_count)
+
+
+    def get_genre(self):
+        return self._genre
+    
+    def set_genre(self, genre):
+        self._genre = genre
+
+    genre = property(get_genre, set_genre)
+
+    def turn_page(self):
+        print("Flipping the page...wow, you read fast!")
+
+
+
+art_of_war = Book("The Art Of War")
+print(art_of_war)
+#art_of_war.set_author("Sun Tzu")
+art_of_war.author = "Sun Tzu"
+print(art_of_war.author)
+art_of_war.set_page_count(260)
+print(art_of_war.page_count)
+print(art_of_war.turn_page())
--- lib/shoe.py
+++ lib/shoe.py
@@ -1,4 +1,42 @@
 #!/usr/bin/env python3
 
 class Shoe:
+
+    def __init__(self, brand, size = 0):
+        self.brand = brand
+        self._size = size
     pass
+
+    def get_size(self):
+        return self._size
+    
+    def set_size(self, size):
+        if isinstance(size, int):
+            self._size = size 
+        else:
+            print("not an integer")
+
+    size = property(get_size, set_size)
+
+    def get_color(self):
+        return self._color
+    
+    def set_color(self, color):
+        self._color = color
+
+    color = property(get_color, set_color)
+
+    def cobble(self):
+        print("Your shoe is as good as new!")
+        self.condition = "New"
+
+    def get_material(self):
+        return self._material
+    
+    def set_material(self, material):
+        self._material = material 
+
+    material = property(get_material, set_material)
+
+le_bron = Shoe("Nike")
+print(le_bron)
