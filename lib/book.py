75fa4df495234f8cc3e2488a5ec5ec9bcbe7a44d
labradorescence <orangenewyorkcreativeagency@gmail.com>
2024-01-11 20:56:27 -0500

    prac

--- lib/book.py
+++ lib/book.py
@@ -1,4 +1,40 @@
-#!/usr/bin/env python3
-
 class Book:
+    def __init__(self, title, author=None, page_count=0, genre=None):
+        self.title = title #attribute 
+        self._page_count = page_count
+        self._genre = genre
     pass
+
+    @property 
+    def author(self):
+        return self._author
+    @author.setter
+    def author(self, author):
+        self._author = author 
+
+    @property
+    def page_count(self):
+        return self._page_count
+    @page_count.setter
+    def page_count(self, page_count):
+        if isinstance(page_count, int):
+            self._page_count = page_count
+        else:
+            print("page_count must be an integer")
+
+    @property
+    def genre(self):
+        return self._genre
+    
+    @genre.setter
+    def genre(self, genre):
+        self._genre = genre
+
+    def turn_page(self):
+        print("Flipping the page...wow, you read fast!")
+
+art_of_war = Book("The Art of War", author="Sun Tzu", page_count=80, genre="self help")
+print(art_of_war.title)
+art_of_war.author ="Sun Tzu"
+print(art_of_war.author)
+    
--- lib/shoe.py
+++ lib/shoe.py
@@ -1,4 +1,47 @@
-#!/usr/bin/env python3
-
 class Shoe:
-    pass
+
+    def __init__(self, brand, size=0):
+        self.brand = brand
+        self._size = size  # instance attribute
+
+    def cobble(self):
+        self.condition = "New"
+        print("Your shoe is as good as new!")
+
+    @property
+    def size(self):
+        return self._size
+    
+    @size.setter
+    def size(self, size):
+        if isinstance(size, int):
+            self._size = size
+        else: 
+            print("Size must be an integer")
+
+    @property
+    def color(self):
+        return self._color
+    
+    @color.setter
+    def color(self, color):
+        self._color = color
+    
+    @property
+    def material(self):
+        return self._material
+    
+    @material.setter
+    def material(self, material):
+        self._material = material
+    
+# Example usage
+le_bron = Shoe("Nike")
+le_bron.size = 14
+print(le_bron.size)
+le_bron.size = "14"  # This will print an error message
+print(le_bron.size)
+le_bron.color = "Dark Green"
+print(le_bron.color)
+le_bron.material = "Leather"
+print(le_bron.material)
